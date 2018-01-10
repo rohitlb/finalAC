e4a7641248e3bc5cac671224995dd014366af212
Rohit Singh <31705884+rohitlb@users.noreply.github.com>
2018-01-10 17:37:38 +0530

    -

---
+++ app.js
@@ -0,0 +1,83 @@
+// require dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var favicon = require('serve-favicon');
+var mongoose = require('mongoose');
+var promise = require('bluebird');
+var session = require('express-session');
+var expressValidator = require('express-validator');
+var cookieParser = require('cookie-parser');
+var mongoDBStore = require('connect-mongodb-session')(session);
+mongoose.Promise = promise;
+//var config = require('./server/config');
+
+//declare the app
+var app = express();
+
+console.log('app');
+
+var store = new mongoDBStore({
+    uri : 'mongodb://localhost/ApniCare',
+    collection : 'mySessions'
+});
+
+store.on('error',function (error) {
+    assert.ifError(error);
+    assert.ok(false);
+});
+
+// to hide X-Powered-By for Security,Save Bandwidth in ExpressJS(node.js)
+app.disable('x-powered-by');
+
+//configure the app
+app.set('port',9000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// adding favicon of Apnicare
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+//set all middleware
+app.use(bodyParser.json());
+//exteended false means it won't be accepting nested objects (accept only single)
+// here security for session to be added like.... session validate
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(expressValidator());
+app.use(express.static(path.join(__dirname,'public')));
+app.use(cookieParser());
+
+// for imagefile in models
+// if saveUninitialized : false than it will store session till the instance is in existence
+// secret is hashing secret
+// secret should be that much complex that one couldnt guess it easily
+app.use(session({
+    secret : 'keyboard cat',
+    cookie : {maxAge : 1000* 60 * 60 * 24 * 7},
+    store : store,
+    resave : false,
+    saveUninitialized : true
+}));
+
+app.get('/',function (req, res) {
+    res.send('hello there!!');
+    res.end();
+});
+
+
+//==========================Database connection===========================
+
+//data base connection and opening port
+var db = 'mongodb://localhost/ApniCare';
+mongoose.connect(db, {useMongoClient: true});
+
+
+//=============================Start server========================
+//connecting database and starting server
+var database = mongoose.connection;
+database.on('open', function () {
+    console.log("database is connected");
+    app.listen(app.get('port'), function () {
+        console.log('server connected to http:localhost:' + app.get('port'));
+    });
+});
